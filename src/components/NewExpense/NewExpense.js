68622a75627a33d8f8067afec5add67abfa58b87
AndrewSimons3 <simdrew07@gmail.com>
2021-04-20 07:54:38 -0500

    added conditional rendering to display form once add new expense button is clicked and hide form once submitted or cancelled

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 
 const NewExpense = (props) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
-    console.log(expenseData);
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditing = () => {
+    setIsEditing(false);
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
+      {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm 
+        onSaveExpenseData={onSaveExpenseDataHandler} 
+        onCancel={stopEditing}/>}
     </div>
   )
 }
